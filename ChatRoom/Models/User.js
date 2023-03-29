4a542693042f9f4b8471f8373f52c4e9f280e002
Atishay Jain(atishay.jain02) <atishay.jain02@infosys.com>
2023-03-29 16:48:10 +0530

    Create basic user model for testing purpose

--- ChatRoom/Models/User.js
+++ ChatRoom/Models/User.js
@@ -1,1 +1,46 @@
+var {dbSetup} = require('./dbConnection');
+const {DataTypes} = require("sequelize");
+var sequelize=dbSetup("chatDB");
 
+exports.User=sequelize.define(
+    "User",
+    {
+      userId:{
+        type: DataTypes.STRING,
+        unique: true,
+        primaryKey: true
+      },
+      name:{
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      age:{
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      gender:{
+        type:DataTypes.ENUM("Male","Female"),
+        allowNull: true
+      },
+      country:{
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      userType:{
+        type: DataTypes.ENUM("Admin","Anonymous","LoggedIn"),
+        allowNull: true
+      },
+      email:{
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+        validate: {
+          isEmail: true
+        } 
+      },
+      loginTime:{
+        type: DataTypes.TIME,
+        allowNull: false
+      }
+    }
+)
--- ChatRoom/server.js
+++ ChatRoom/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const {dbSetup} = require('./Models/dbConnection');
+const {User} = require('./Models/User')
 const app = express();
 
 app.listen(5000,()=>{ 
-    dbSetup("chatDB");
+    // dbSetup("chatDB");
     console.log("Listening at port 5000")
+    sequelize.sync().then(() => {
+
+        User.findAll().then(res => {
+            console.log("Data of user",res)
+        }).catch((error) => {
+            console.error('Failed to retrieve data : ', error);
+        });
+      
+      }).catch((error) => {
+        console.error('Unable to create table : ', error);
+      });
 });
 
 module.exports = app;

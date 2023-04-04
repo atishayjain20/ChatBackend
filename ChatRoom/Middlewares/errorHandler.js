0218bc9372e8e92279b42fe0293892216d03398c
Atishay Jain(atishay.jain02) <atishay.jain02@infosys.com>
2023-04-04 17:33:47 +0530

    DB ValidationError Implementation

--- ChatRoom/Middlewares/errorHandler.js
+++ ChatRoom/Middlewares/errorHandler.js
@@ -7,6 +7,13 @@ const globalErrorHandler = (err,req,res,next)=>{
                 error: err
             })
             break;
+        case err.message === "Validation error":
+            res.status(400).json({
+                success: false,
+                message: "Validation error occurred",
+                errors: Object.values(err.errors).map((errObj) => errObj.message),
+            });
+            break;
         default:
             errorRes.json({
                 status: err.status || 500,
